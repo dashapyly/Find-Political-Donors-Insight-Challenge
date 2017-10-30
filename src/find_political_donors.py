10f383da1d04df126e53742170af16237adb48cb
Dar'ya Redka <dasha.pylypenko@gmail.com>
2017-10-29 23:53:45 -0400

    Add DataFrame loader functions

--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -1,6 +1,17 @@
+from typing import Iterator
+
 import pandas as pd
 
 from running_median import RunningMedian
+
+
+COLUMN_NAMES = [
+    'CMTE_ID', 'AMNDT_IND', 'RPT_TP', 'TRANSACTION_PGI', 'IMAGE_NUM', 'TRANSACTION_TP', 'ENTITY_TP',
+    'NAME', 'CITY', 'STATE', 'ZIP_CODE', 'EMPLOYER', 'OCCUPATION', 'TRANSACTION_DT', 'TRANSACTION_AMT',
+    'OTHER_ID', 'TRAN_ID', 'FILE_NUM', 'MEMO_CD', 'MEMO_TEXT', 'SUB_ID'
+]
+COLUMN_TYPES = {'ZIP_CODE': str, 'TRANSACTION_DT': str}
+USECOLS = ['CMTE_ID', 'ZIP_CODE', 'TRANSACTION_DT', 'TRANSACTION_AMT', 'OTHER_ID']
 
 
 def calculate_statistics_by_zip_code(input_stream: pd.DataFrame) -> pd.DataFrame:
@@ -27,6 +38,17 @@ def calculate_statistics_by_date(input_df: pd.DataFrame) -> pd.DataFrame:
     ...
 
 
+def iter_input_file(input_file: str, chunksize: int=100_000) -> Iterator[pd.DataFrame]:
+    input_stream = pd.read_csv(
+        input_file, sep='|', names=COLUMN_NAMES, usecols=USECOLS, dtype=COLUMN_TYPES, chunksize=chunksize)
+    return input_stream
+
+
+def read_input_file(input_file: str) -> pd.DataFrame:
+    input_df = pd.read_csv(input_file, sep='|', names=COLUMN_NAMES, usecols=USECOLS, dtype=COLUMN_TYPES)
+    return input_df
+
+
 def main(input_file: str, output_file_zip: str, output_file_date: str) -> int:
     """Calculate statistics for federal election campaign contributions.
 
@@ -38,9 +60,16 @@ def main(input_file: str, output_file_zip: str, output_file_date: str) -> int:
     Returns:
         System return code.
     """
-    ...
+    for chunk in iter_input_file(input_file):
+        chunk_zip_stats = calculate_statistics_by_zip_code(chunk)
+        chunk_zip_stats.to_csv(output_file_zip, mode='a', sep='|', index=False, header=False)
+
+    input_df = read_input_file(input_file)
+    date_stats = calculate_statistics_by_date(input_df)
+    date_stats.to_csv(output_file_date, mode='a', sep='|', index=False, header=False)
 
 
 if __name__ == '__main__':
     import sys
+
     sys.exit(main())
